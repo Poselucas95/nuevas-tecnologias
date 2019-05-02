2c05a97a56dbecb2f7e6f4d8c0e33b6938014c5a
Lucas95 <poselucas43@gmail.com>
2019-05-02 00:49:07 -0300

    Validaciones completas.
    EMAIL - NOMBRE - APELLIDO - DNI
    Valida lo anteriormente dicho en el evento submit

--- js/script.js
+++ js/script.js
@@ -11,19 +11,6 @@ function bindearEventoTerminosYCondiciones() {
     $(".texto-terminos").click(mostrarTerminos);
 }
 
-function validarCampos() {
-    validarDniIngresado();
-    validarTerminos();
-}
-
-function validarDniIngresado() {
-    var DNI = document.getElementById("inputDni").value;
-    if (DNI.length != 8) {
-        alert("El DNI debe tener 8 caracteres");
-    }
-    event.cancelBubble = true;
-}
-
 
 
 function validarTerminos() {
@@ -36,18 +23,34 @@ function mostrarTerminos() {
     $("#modalTerminos").css('display', 'inline-block');
 }
 
-document.getElementById("email").addEventListener("input", function () {
-    var email = document.getElementById("email");
-    var val = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if (val.test(email.value)) {
-        email.classList.remove("is-invalid");
-        email.classList.add("is-valid");
-    } else {
-        email.classList.remove("is-valid");
-        email.classList.add("is-invalid");
-    }
+
+
+//Validación del nombre en linea
+document.getElementById("inputNombre").addEventListener("input", function () {
+    var nombre = document.getElementById("inputNombre");
+    //Expresión regular que debe poseer el texto ingresado
+    var val = /^([a-zA-Z]+[\s]*)+$/;
+    validarExpresion(nombre, val);
 });
 
+//Validación del apellido en linea
+document.getElementById("inputApellido").addEventListener("input", function () {
+    var apellido = document.getElementById("inputApellido");
+    //Expresión regular que debe poseer el texto ingresado
+    var val = /^([a-zA-Z]+[\s]*)+$/;
+    validarExpresion(apellido, val)
+});
+
+    
+//Validación del correo en linea
+document.getElementById("inputEmail").addEventListener("input", function () {
+    var email = document.getElementById("inputEmail");
+    //Expresión regular que debe poseer el texto ingresado
+    var val = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    validarExpresion(email, val);
+});
+
+//Validación del DNI en linea
 document.getElementById("inputDni").addEventListener("input", function () {
     var dni = document.getElementById("inputDni");
     if (dni.value.length > 8 || dni.value.length < 7) {
@@ -60,15 +63,90 @@ document.getElementById("inputDni").addEventListener("input", function () {
 
 })
 
+
+//Validación del formulario al momento de ser enviado (enter o click)
 function validarFormulario() {
-    if (document.getElementById("email").classList.contains("is-invalid")) {
-        alert("Email incorrecto");
+    //Variables de las ID de los campos a validar
+    var inputEmail = document.getElementById("inputEmail");
+    var inputDni = document.getElementById("inputDni");
+    var inputNombre = document.getElementById("inputNombre");
+    var inputApellido = document.getElementById("inputApellido");
+
+    //Validación del Email en caso de ser vacio o poseer la clase "is-invalid"(bootstrap)
+    if (inputEmail.classList.contains("is-invalid") || inputEmail.value.length < 1) {
         event.preventDefault();
+        var campoEmail = document.querySelector(".email");
+        var textoEmail = "Debe ingresar un correo electronico valido";
+
+        if (document.querySelector(".errorEmail")) {
+            document.querySelector(".errorEmail").innerText = textoEmail;
+        } else {
+            crearAlertaError("errorEmail", campoEmail, textoEmail);
+        }
+    } else {
+        //Eliminar alerta en caso de ser corregido
+        document.querySelector(".errorEmail").parentNode.removeChild(document.querySelector(".errorEmail"));
     }
+    
+    //Validación del DNI en caso de ser vacio o poseer la clase "is-invalid"(bootstrap)
+    if (inputDni.classList.contains("is-invalid") || inputDni.value.length < 1) {
+        event.preventDefault();
+        var campoDni = document.querySelector(".dni");
+        var textoDni = "Debe tener entre 7 y 8 caracteres";
+
+        if (document.querySelector(".errorDni")) {
+            document.querySelector(".errorDni").innerText = textoDni;
+        } else {
+            crearAlertaError("errorDni", campoDni, textoDni);
+        }
+    } else {
+        //Eliminar alerta en caso de ser corregido
+        document.querySelector(".errorDni").parentNode.removeChild(document.querySelector(".errorDni"));
+    }
+
+    //Validación del nombre y apelldio en caso de ser vacio o poseer la clase "is-invalid"(bootstrap)
+    if (inputNombre.classList.contains("is-invalid") || inputNombre.value.length < 1 || inputApellido.classList.contains("is-invalid") || inputApellido.value.length < 1) {
+        event.preventDefault();
+        var campoNombreApellido = document.querySelector(".nombreApellido");
+        var textoNombreApellido = "Ingrese su nombre y apellido";
+
+        if (document.querySelector(".errorNombreApellido")) {
+            document.querySelector(".errorNombreApellido").innerText = textoNombreApellido;
+        } else {
+            crearAlertaError("errorNombreApellido", campoNombreApellido, textoNombreApellido);
+        }
+    } else {
+        //Eliminar alerta en caso de ser corregido
+        document.querySelector(".errorNombreApellido").parentNode.removeChild(document.querySelector(".errorNombreApellido"));
+    }
+
 
 }
 
+//Función para la creación de alertas.
+function crearAlertaError(clase, contenedor, texto) {
+    var fragmento = document.createDocumentFragment();
+    var div = document.createElement("div");
+    //Se añaden las clases correspondientes y el texto a mostrar.
+    div.classList.add("alert-danger", "alert", clase);
+    div.setAttribute("role", "alert");
+    div.innerText = texto;
+    //Se incluyen los elementos creados.
+    fragmento.appendChild(div);
+    contenedor.appendChild(fragmento);
+}
 
+
+//Función para validar la expresion regular.
+function validarExpresion(valor, expre) {
+    if (expre.test(valor.value)) {
+        valor.classList.remove("is-invalid");
+        valor.classList.add("is-valid");
+    } else {
+        valor.classList.remove("is-valid");
+        valor.classList.add("is-invalid");
+    }
+}
 
 
 $(document).ready(init); 

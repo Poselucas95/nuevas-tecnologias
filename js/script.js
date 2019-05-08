84cdf8b4873ecce05c0fd94fefe22586e213bb8f
Fran Agraz <franzferdinand.agraz@hotmail.com>
2019-05-08 14:18:42 -0300

    Intento de validar checkbox

--- js/script.js
+++ js/script.js
@@ -46,6 +46,7 @@ function validarFormulario() {
     var inputDni = document.getElementById("inputDni");
     var inputNombre = document.getElementById("inputNombre");
     var inputApellido = document.getElementById("inputApellido");
+    var checkBox = document.getElementById("TYC");
 
     //Validación del Email en caso de ser vacio o poseer la clase "is-invalid"(bootstrap)
     if (inputEmail.classList.contains("is-invalid") || inputEmail.value.length < 1) {
@@ -95,6 +96,17 @@ function validarFormulario() {
         document.querySelector(".errorNombreApellido").parentNode.removeChild(document.querySelector(".errorNombreApellido"));
     }
 
+    //función para validar el checkbox de los términos y condiciones    
+    if (!checkBox.checked) {
+        event.preventDefault();
+        var fragmento = document.createDocumentFragment();
+        var div = document.createElement("div");
+        div.classList.add("alert-danger", "alert");
+        div.setAttribute("role", "alert");
+        div.innerText = "los términos y condiciones deben ser aceptados para continuar.";
+        fragmento.appendChild(div);
+        checkBox.appendChild(fragmento);
+    }
 
 }
 
@@ -123,29 +135,17 @@ function validarExpresion(valor, expre) {
     }
 }
 
-
-
-
-// Get the modal
 var modal = document.getElementById('modalTyc');
-
-// Get the button that opens the modal
 var tyc = document.getElementById("tyc");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
 tyc.onclick = function () {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target === modal) {
         modal.style.display = "none";

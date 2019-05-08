90ea5246582f0f1651d26a2781a71927f89246b0
Bautista Querejeta <41586620@ort>
2019-05-08 19:58:44 -0300

    agregar

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@
 
 
 
+var jsonProvinciasYLocalidades;
+
+document.addEventListener("DOMContentLoaded", function () {
+    obtenerYAgregarListaProvincias();
+}, false);
+
+
+
 
 function loadJSON(jsonName, callback) {
 
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'http://myjson.com/za2z0', true);
+    xobj.open('GET', jsonName);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode

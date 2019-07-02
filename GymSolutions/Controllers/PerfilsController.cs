facabbb03dd541b403eec0da365bdd1557033ac8
Lucas95 <poselucas43@gmail.com>
2019-07-02 16:51:32 -0300

    Base de datos corregida.
    Añadida la base de datos, también las view y controllers de todas las tablas básicas de uso.

---
+++ GymSolutions/Controllers/PerfilsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GymSolutions.Models;
+
+namespace GymSolutions.Controllers
+{
+    public class PerfilsController : Controller
+    {
+        private GymSolutionEntities1 db = new GymSolutionEntities1();
+
+        // GET: Perfils
+        public ActionResult Index()
+        {
+            var perfil = db.Perfil.Include(p => p.Peso_Usuario);
+            return View(perfil.ToList());
+        }
+
+        // GET: Perfils/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Perfil perfil = db.Perfil.Find(id);
+            if (perfil == null)
+            {
+                return HttpNotFound();
+            }
+            return View(perfil);
+        }
+
+        // GET: Perfils/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_PERFIL = new SelectList(db.Peso_Usuario, "ID_PESO", "ID_PESO");
+            return View();
+        }
+
+        // POST: Perfils/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_PERFIL,perfilNombre,perfilApellido,perfilEdad,perfilAltura")] Perfil perfil)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Perfil.Add(perfil);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_PERFIL = new SelectList(db.Peso_Usuario, "ID_PESO", "ID_PESO", perfil.ID_PERFIL);
+            return View(perfil);
+        }
+
+        // GET: Perfils/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Perfil perfil = db.Perfil.Find(id);
+            if (perfil == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_PERFIL = new SelectList(db.Peso_Usuario, "ID_PESO", "ID_PESO", perfil.ID_PERFIL);
+            return View(perfil);
+        }
+
+        // POST: Perfils/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_PERFIL,perfilNombre,perfilApellido,perfilEdad,perfilAltura")] Perfil perfil)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(perfil).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_PERFIL = new SelectList(db.Peso_Usuario, "ID_PESO", "ID_PESO", perfil.ID_PERFIL);
+            return View(perfil);
+        }
+
+        // GET: Perfils/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Perfil perfil = db.Perfil.Find(id);
+            if (perfil == null)
+            {
+                return HttpNotFound();
+            }
+            return View(perfil);
+        }
+
+        // POST: Perfils/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Perfil perfil = db.Perfil.Find(id);
+            db.Perfil.Remove(perfil);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GymSolutions/Controllers/RutinasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GymSolutions.Models;
+
+namespace GymSolutions.Controllers
+{
+    public class RutinasController : Controller
+    {
+        private GymSolutionEntities1 db = new GymSolutionEntities1();
+
+        // GET: Rutinas
+        public ActionResult Index()
+        {
+            var rutina = db.Rutina.Include(r => r.Perfil);
+            return View(rutina.ToList());
+        }
+
+        // GET: Rutinas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rutina rutina = db.Rutina.Find(id);
+            if (rutina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rutina);
+        }
+
+        // GET: Rutinas/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_PERFIL = new SelectList(db.Perfil, "ID_PERFIL", "perfilNombre");
+            return View();
+        }
+
+        // POST: Rutinas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_RUTINA,rutNombre,rutFechaCreacion,rutFechaModificacion,ID_PERFIL,usuCreador")] Rutina rutina)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Rutina.Add(rutina);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_PERFIL = new SelectList(db.Perfil, "ID_PERFIL", "perfilNombre", rutina.ID_PERFIL);
+            return View(rutina);
+        }
+
+        // GET: Rutinas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rutina rutina = db.Rutina.Find(id);
+            if (rutina == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_PERFIL = new SelectList(db.Perfil, "ID_PERFIL", "perfilNombre", rutina.ID_PERFIL);
+            return View(rutina);
+        }
+
+        // POST: Rutinas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_RUTINA,rutNombre,rutFechaCreacion,rutFechaModificacion,ID_PERFIL,usuCreador")] Rutina rutina)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rutina).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_PERFIL = new SelectList(db.Perfil, "ID_PERFIL", "perfilNombre", rutina.ID_PERFIL);
+            return View(rutina);
+        }
+
+        // GET: Rutinas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rutina rutina = db.Rutina.Find(id);
+            if (rutina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rutina);
+        }
+
+        // POST: Rutinas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Rutina rutina = db.Rutina.Find(id);
+            db.Rutina.Remove(rutina);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

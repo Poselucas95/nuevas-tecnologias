1a52c69b2f772002183407c2fd422cd05b9242a5
Poselucas95 <33238193+Poselucas95@users.noreply.github.com>
2019-10-30 20:57:39 -0300

    New

---
+++ GymSolutions/Controllers/RoutinesController.cs
@@ -0,0 +1,41 @@
+﻿using GymSolutions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GymSolutions.Controllers
+{
+    public class RoutinesController : Controller
+    {
+        private Entities1 contexto = new Entities1();
+
+
+        // GET: Routines
+        public ActionResult Index()
+        {
+            var rutinas = contexto.Rutina.ToList();
+
+            return View(rutinas);
+        }
+
+        public ActionResult VerRutina()
+        {
+            return View();
+        }
+
+        public ActionResult NuevaRutina(int ejer = 1)
+        {
+
+
+            return View();
+        }
+
+        public ActionResult EditarRutina()
+        {
+            return View();
+        }
+
+    }
+}

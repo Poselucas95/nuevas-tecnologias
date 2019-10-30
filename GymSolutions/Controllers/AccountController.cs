b4686490750e8a6ac0ed94a485f37adcdc66d86c
Poselucas95 <33238193+Poselucas95@users.noreply.github.com>
2019-10-30 20:26:17 -0300

    Base de datos modificada

--- GymSolutions/Controllers/AccountController.cs
+++ GymSolutions/Controllers/AccountController.cs
@@ -157,7 +157,7 @@ namespace GymSolutions.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    Entities db = new Entities();
+                    
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                  
                     // Para obtener más información sobre cómo habilitar la confirmación de cuentas y el restablecimiento de contraseña, visite https://go.microsoft.com/fwlink/?LinkID=320771
